326c38a6d96df9a612b894817e9e7b50142eac21
unknown <kyleedwardgjava@gmail.com>
2020-10-13 10:53:53 -0700

    Finished Workshop

--- IntroToPython/intro.py
+++ IntroToPython/intro.py
@@ -1,12 +1,70 @@
-students = [
-    {
-        'name': 'kyle',
-        'age': 20
-    },
-    {
-        'name': 's',
-        'age': 5
-    }
-]
 
-print(students[1]['name']      )
+#Print statements
+#print("Hello!")
+#print("My name is kyle")
+#print("I am 20 years old.")
+
+
+#Variables
+#my_name = "Kyle"
+#print("Hello " + my_name + ".")
+
+#my_age = 20
+#print("I am " + str(my_age) + " years old.")
+
+#my_age_after_10_years = my_age + 10
+#print("After 10 years, i am  " + str(my_age_after_10_years))
+
+#Conditional statements
+#num1 = 3
+#num2 = 3
+#if(num1 > num2):
+#    print("test 1")
+#elif(num1 == num2):
+#    print("They are equal")
+#else:
+#    print("test2")
+
+#User input
+#input_name = input("What is your name? ")
+#print("Nice to meet you, " + input_name)
+
+#comp_age = 20
+#input_age = int(input("What is your age? "))
+#print("you are " + str(input_age) + " years old.")
+
+#if(input_age > comp_age):
+#    print("You are older than me")
+#elif(input_age < comp_age):
+#    print("I am older")
+#elif(input_age == comp_age):
+#    print("We are the same")
+
+
+#Arrays & Lists
+#shopping_list = ["apples" , "berries", "bananas"]
+#shopping_list.append("melons")
+#size_of_list = len(shopping_list)
+#for i in range(size_of_list):
+#    print(shopping_list[i])
+
+#functions
+#num1 = 5
+#num2 = 3
+
+#def AddTwoNumbers(num1 , num2):
+#    sum = num1 + num2
+#    print(sum)
+#    return sum
+
+#num3 = AddTwoNumbers(num1 , num2)
+#print(num3)
+
+
+#Maps
+#student = {
+#    'name':'kyle',
+#    'age': 20,
+#    'school': 'CSUSM'
+#}
+#print(student['school'])
