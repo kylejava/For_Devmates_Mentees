5580bab9be7dd7a640db0fc3ebab2892075402ea
unknown <kyleedwardgjava@gmail.com>
2020-10-18 00:48:53 -0700

    Figured out how to transfer from one file to another

--- WeatherWebsiteWithFlask/main.py
+++ WeatherWebsiteWithFlask/main.py
@@ -1,15 +1,20 @@
 import requests
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect
 app = Flask(__name__)
 
 
 @app.route('/' , methods = ['GET' , 'POST'])
 def home():
+    if(request.method == 'POST'):
+        city = request.form.get('city')
+        print(city + " test")
     return render_template('index.html')
 
 @app.route('/result' , methods = ['GET' , 'POST'])
 def result():
-    return render_template('result.html')
+    user_city = request.args.get('city',None)
+
+    return render_template('result.html' , city = user_city)
 
 
 if __name__ == "__main__":
