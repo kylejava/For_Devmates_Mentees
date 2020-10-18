02ffc8439750a8a0b9757d57d739312565b1c548
unknown <kyleedwardgjava@gmail.com>
2020-10-17 20:18:49 -0700

    Updated IntroToFlask workshop. Website now converts degrees

---
+++ IntroToFlask/functions.py
@@ -0,0 +1,9 @@
+
+def ConvertCToF(degree):
+
+    ans = ((degree*(9/5))+32)
+    return ans
+
+def ConvertFToC(degree):
+    ans = ((degree - 32) *(5/9))
+    return ans
--- IntroToFlask/main.py
+++ IntroToFlask/main.py
@@ -1,10 +1,24 @@
 import requests
+from functions import *
 from flask import Flask, render_template, flash, request
 app = Flask(__name__)
 
 @app.route('/' , methods = ['GET' , 'POST'])
 def home():
-    return render_template('index.html')
+    deg = ""
+    con_deg = ""
+
+    if(request.method == 'POST'):
+        num = request.form['degree']
+        deg = ("Entered Degree: " + str(num))
+        choice = request.form.get('degree_type')
+        if(choice == 'F'):
+            converted_num = ConvertCToF(float(num))
+        elif(choice == 'C'):
+            converted_num = ConvertFToC(float(num))
+        con_deg = ("Converted Degree to " + choice + " " + str(converted_num))
+
+    return render_template('index.html',degree = deg, converted_degree = con_deg)
 
 
 if __name__ == "__main__":
